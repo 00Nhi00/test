618b92602fc96c2b7e38c95ec3438f56dcc31cf0
Nhi <thaonhi.nguyen411@gmail.com>
2022-10-24 06:10:14 -0700

    nhi portal page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MyPortal page</title>
+  <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <h1><a href="index.php">Nhi's Portal Page</a></h1> 
+  <h2>The navigation below represents our BIG Assignments. </h2>
+    <div id="wrapper"> 
+  <nav>
+    <ul>
+      <li><a href="website/index.html">Switch</a><li>
+      <li><a href="">Troubleshoot</a><li>
+      <li><a href="">Calculator</a><li>
+      <li><a href="">Email</a><li>
+      <li><a href="">Database</a><li>
+      <li><a href="">Gallery</a><li>
+</ul>
+</nav>
+<main>
+<h2>About Nhi</h2>
+<img class="right" src="images/nhi.jpg" alt="Nhi">
+<p>Hi, my name is Nhi(Rosie), I'm in the third quarter of my Web Development program at Seattle Central College. I like watching shows, cooking and hanging out with friends and family in my free time.<p>
+<img class="mamp_screenshot" src="images/mamp_localhost.jpg" alt="mamp_localhost">
+<img class="mamp_screenshot" src="images/test_mamp.jpg" alt="test_mamp">
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ol>
+  <li><a href="weeks/week2/var.php">var.php</a></li>
+  <li><a href="weeks/week2/var2.php">var2.php</a></li>
+  <li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+  <li><a href="weeks/week2/currency.php">currency.php</a></li>
+</ol>
+
+</aside>
+
+</body>
+</html>
---
+++ weeks/week2/var.php
@@ -0,0 +1,51 @@
+<?php
+// my variables
+
+$name= "Nhi"; // Nhi is a string
+$name='Hattie'; 
+echo $name;
+
+$body_temp = 98; // 98 is an integer
+$body_temp_new = 98.6; // 98.6 is a float
+echo '<br>';
+echo "<br>";
+echo $body_temp;
+echo "<br>";
+echo $body_temp_new;
+echo "<br>";
+echo "My name is $name!";
+echo "<br>";
+echo 'My name is $name!';//I like the single quotes
+
+echo "<br>";
+echo 'My name is '.$name.' !';//I like the single quotes
+
+echo "<br>";
+echo 'The normal body temperature for a human being is '.$body_temp_new.' ';
+echo "<br>";
+$name='Sharon';
+echo $name;
+$x = 20;
+$y = 5;
+
+$z = $x + $y; 
+echo $z;
+echo "<br>";
+$z = $x * $y;
+echo $z;
+echo "<br>";
+$z = $x / $y;
+echo $z;
+
+echo "<br>";
+
+$first_name = 'Nhi';
+$last_name = 'Nguyen';
+echo $first_name." ".$last_name;
+echo $first_name.' '.$last_name;
+
+echo "<br>";
+echo "My full name is $first_name $last_name";
+echo "<br>";
+echo 'My full name is '.$first_name.' '.$last_name;
+
